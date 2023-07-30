e193e15c20bc0ad1c6ed0496b114f6a2cf63d69a
Praveen Shinde <shindepraveen002@gmail.com>
2023-07-30 22:04:34 +0530

    added external in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { externals: ["aws-sdk", "chrome-aws-lambda"] };
 
-module.exports = nextConfig
+module.exports = nextConfig;
